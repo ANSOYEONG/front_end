a8c502f01b8813002e1aa6441e17c0edb9658554
ANSOYEONG <mongsyo@gmail.com>
2016-07-25 17:54:45 +0900

    07/25 월요일

---
+++ front_end-master/class/webpage/js/gnb.js
@@ -0,0 +1,23 @@
+/**
+ * Created by 321-01 on 2016-07-22.
+ */
+
+$(function(){
+    $('.gnb-depth1-link').on('mouseenter' , function(){
+
+        $('.gnb-depth2').stop().delay(500).fadeIn(500);
+        $('.gnb-bg').stop().animate({
+            height:274
+        });
+    });
+
+    $('.gnb-depth1-link').on('mouseleave' , function(){
+
+        $('.gnb-depth2').stop().fadeOut();
+        $('.gnb-bg').stop().animate({
+            height:0
+        });
+    });
+
+});
+
